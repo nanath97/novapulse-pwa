2e148e7af17aa0172fc09912a43a311f08ba6b94
nathan <vinteo.ac@gmail.com>
2026-05-30 11:48:57 +0200

    changement adresse url

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { io } from "socket.io-client";
 
 
 
-const BRIDGE_URL = "https://novapulse-bridge.onrender.com";
+const BRIDGE_URL = "https://mini-jessie-bot-1.onrender.com";
 
 function App() {
   
